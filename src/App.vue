fcf4eb00f315a60caff07c097efb8406eb3f4df7
panda <yourhan1996@outlook.com>
2019-04-12 10:09:08 +0800

    vue demo one

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+// 模板：HTML结构
+<template>
+  <div id="app">
+    <img src="./assets/panda.png" class="logo">
+    <one/>
+  </div>
+</template>
+
+// 行为：处理逻辑
+<script>
+import one from "./components/one";
+
+export default {
+  name: "App",
+  components: {
+    one
+  }
+};
+</script>
+
+// 样式：解决样式
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-image: url("./assets/rain.png");
+  /* background-size: 100% 100%; */
+  background-attachment: fixed;
+}
+.logo {
+  margin: 8% 0 0 0;
+}
+</style>
